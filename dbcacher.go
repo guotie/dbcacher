302131dd2f6605e9179fc59b851a330c62f31e16
guotie <guotie.9@gmail.com>
2013-12-12 17:06:16 +0800

    test pass

--- dbcacher.go
+++ dbcacher.go
@@ -3,7 +3,7 @@ package dbcacher
 import (
 	//"bytes"
 	"database/sql"
-	"encoding/gob"
+	//"encoding/gob"
 	"fmt"
 	"github.com/garyburd/redigo/redis"
 	"log"
@@ -12,26 +12,30 @@ import (
 	"time"
 )
 
-type cacher interface {
-	Get()
-	Set()
-}
+const (
+	DefaultFlushDB = 300
+	DefaultExpires = 3600
+)
 
 type Modeler interface {
 	RowToModel(row *sql.Row) error
-	gob.GobDecoder
-	gob.GobEncoder
+	UpdateClause() (string, error)
+	Decode([]byte) error
+	Encode() ([]byte, error)
 }
 
 type expirestmr struct {
-	d   time.Duration
-	tmr *time.Timer
+	fd      time.Duration // flush to db
+	ed      time.Duration // delete from redis
+	flushdb *time.Timer
+	expires *time.Timer
 }
+
 type DbCacher struct {
 	lock sync.Mutex
 	db   *sql.DB
 	pool *redis.Pool
-	tmr  map[string]expirestmr
+	tmr  map[string]*expirestmr
 }
 
 var (
@@ -46,7 +50,7 @@ func Create(db *sql.DB, pool *redis.Pool) {
 
 	dbc = DbCacher{db: db,
 		pool: pool,
-		tmr:  make(map[string]expirestmr),
+		tmr:  make(map[string]*expirestmr),
 	}
 
 	created = true
@@ -55,60 +59,152 @@ func Create(db *sql.DB, pool *redis.Pool) {
 func Get(m Modeler, table string, id int, timeout int) error {
 	t := reflect.TypeOf(m)
 	if t.Kind() != reflect.Struct {
-		panic("Modeler m must be type struce!\n")
+		//panic("Modeler m must be type struce!\n")
 	}
 
 	key := fmt.Sprintf("%s-%d", table, id)
+	if timeout <= 0 {
+		timeout = DefaultExpires
+	}
+
 	dbc.lock.Lock()
 	val, ok1 := dbc.pool.Get().Do("HGET", table, id)
 	if ok1 != nil || val == nil {
+		// 在redis中没有找到该数据，从数据库中获取
 		dbc.lock.Unlock()
 		err := queryrow(m, table, id)
 		if err != nil {
 			return err
 		}
-		var buf []byte
-		buf, err = m.GobEncode()
+
+		// 将结果保存到redis中
+		err = Set(m, table, id, DefaultFlushDB)
 		if err != nil {
 			return err
 		}
 
-		// 写入到redis缓存中
+		// 设置该数据在redis中超时时间
 		dbc.lock.Lock()
-		_, err = dbc.pool.Get().Do("HSET", table, id, buf)
-		if err != nil {
-			log.Printf("HSET %s %d failed: %s!", table, id, err.Error())
+		etmr, _ := dbc.tmr[key]
+		tmo := time.Duration(timeout) * time.Second
+		etmr.ed = tmo
+		if etmr.expires != nil {
+			etmr.expires.Reset(tmo)
 		} else {
-			tmo := time.Duration(timeout) * time.Second
-			tmr := time.AfterFunc(tmo, func() {
-				dbc.pool.Get().Do("HDEL", table, id)
+			etmr.expires = time.AfterFunc(tmo, func() {
+				dbc.lock.Lock()
+				dbc.pool.Get().Do("HDel", table, key)
+				dbc.lock.Unlock()
 			})
-			dbc.tmr[key] = expirestmr{
-				d:   tmo,
-				tmr: tmr,
-			}
 		}
 		dbc.lock.Unlock()
 		return nil
 	}
 
 	etmr, ok2 := dbc.tmr[key]
+	ed := time.Duration(timeout) * time.Second
 	if ok2 {
-		etmr.tmr.Reset(etmr.d)
+		etmr.ed = ed
+		etmr.expires.Reset(etmr.ed)
 	} else {
 		log.Printf("Not found timer for %s-%d\n", table, id)
+		etmr = &expirestmr{ed: ed,
+			expires: time.AfterFunc(ed, func() {
+				dbc.lock.Lock()
+				dbc.pool.Get().Do("HDel", table, key)
+				dbc.lock.Unlock()
+			}),
+		}
+		dbc.tmr[key] = etmr
 	}
 	dbc.lock.Unlock()
 
-	err := m.GobDecode(val.([]byte))
+	err := m.Decode(val.([]byte))
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
+// 将数据写入到redis中，并设置timer到期时，更新数据库
+func Set(m Modeler, table string, id int, timeout int) (err error) {
+	var buf []byte
+
+	buf, err = m.Encode()
+	if err != nil {
+		return err
+	}
+
+	clause, cerr := m.UpdateClause()
+	key := fmt.Sprintf("%s-%d", table, id)
+	if timeout <= 0 {
+		timeout = DefaultFlushDB
+	}
+
+	// 写入到redis缓存中
+	dbc.lock.Lock()
+	_, err = dbc.pool.Get().Do("HSET", table, id, buf)
+	if err != nil {
+		log.Printf("HSET %s %d failed: %s!", table, id, err.Error())
+	} else {
+		if cerr != nil {
+			goto unlock
+		}
+		// update 数据库操作
+		flushTmo := time.Duration(timeout) * time.Second
+		ftmr, ok := dbc.tmr[key]
+		if !ok {
+			ftmr = &expirestmr{fd: flushTmo}
+			dbc.tmr[key] = ftmr
+		}
+		flushTmr := ftmr.flushdb
+
+		flushTmr = ftmr.flushdb
+		if flushTmr != nil {
+			flushTmr.Reset(flushTmo)
+		} else {
+			flushTmr = time.AfterFunc(flushTmo, func() {
+				println("exec update:", clause)
+				_, err := dbc.db.Exec(clause)
+				if err != nil {
+					log.Printf("Exec update clause %s failed: %s\n",
+						clause, err.Error())
+				}
+			})
+
+		}
+
+	}
+unlock:
+	dbc.lock.Unlock()
+
+	return nil
+}
+
+// 立刻将数据写入到数据库中
+func Flush(m Modeler, table string, id int) error {
+	clause, cerr := m.UpdateClause()
+	key := fmt.Sprintf("%s-%d", table, id)
+	if cerr == nil {
+		_, err := dbc.db.Exec(clause)
+		if err != nil {
+			log.Printf("Flush: exec clause %s failed: %s\n", clause, err.Error())
+		}
+	}
+	dbc.lock.Lock()
+	tmr, ok := dbc.tmr[key]
+	if ok {
+		if tmr.flushdb != nil {
+			tmr.flushdb.Stop()
+		}
+	}
+	dbc.lock.Unlock()
+	return nil
+}
+
 func queryrow(m Modeler, table string, id int) error {
-	row := dbc.db.QueryRow(fmt.Sprintf("SELECT * FROM %s WHERE id=%d;", table, id))
+	query := fmt.Sprintf("SELECT * FROM %s WHERE id=%d;", table, id)
+	row := dbc.db.QueryRow(query)
 	err := m.RowToModel(row)
 	return err
 }
--- dbcacher_test.go
+++ dbcacher_test.go
@@ -1,1 +1,145 @@
 package dbcacher
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/gob"
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	_ "github.com/lib/pq"
+	"testing"
+	"time"
+)
+
+type User struct {
+	Id       int64
+	Name     string
+	Passwd   string
+	Descp    string
+	Created  int64
+	Birthday int64
+}
+
+func (u *User) RowToModel(row *sql.Row) error {
+	err := row.Scan(&u.Id, &u.Name, &u.Passwd,
+		&u.Descp, &u.Created, &u.Birthday)
+	println("rowtomodel: ", u.Id, u.Name, u.Passwd, u.Descp)
+	return err
+}
+
+func (u *User) Decode(data []byte) error {
+	println("Decode:", u.Id, u.Name, u.Passwd, u.Descp)
+	buf := bytes.NewBuffer(data)
+	dec := gob.NewDecoder(buf)
+
+	return dec.Decode(&u)
+}
+
+func (u *User) Encode() ([]byte, error) {
+	var buf bytes.Buffer
+	println("Encode:", u.Id, u.Name, u.Passwd, u.Descp)
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(u)
+	if err != nil {
+		println(err.Error())
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+func (u *User) UpdateClause() (string, error) {
+	var clause = "UPDATE users SET %s='%s', %s='%s', %s='%s', %s=%d, %s=%d WHERE id=%d"
+
+	uc := fmt.Sprintf(clause, "name", u.Name, "passwd", u.Passwd,
+		"descp", u.Descp, "created", u.Created, "birthday", u.Birthday, u.Id)
+	return uc, nil
+}
+
+func openRedis() *redis.Pool {
+	pool := &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 600 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", "127.0.0.1:6379")
+			if err != nil {
+				panic(err)
+				return nil, err
+			}
+
+			return c, nil
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+	return pool
+}
+
+func openDB() *sql.DB {
+	db, err := sql.Open("postgres",
+		"user=postgres dbname=pgtest password=action sslmode=disable")
+	if err != nil {
+		panic(err.Error())
+	}
+	drop := "drop table users;"
+	db.Exec(drop)
+
+	create := "create table if not exists users (id int8, name varchar(32), passwd varchar(64), descp varchar(200), created int8, birthday int8)"
+	_, err = db.Exec(create)
+	if err != nil {
+		panic(err.Error())
+	}
+	_, err = db.Exec("insert into users values(1, 'guotie', '1234', 'a coder in china', 23, 2300)")
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+func _test_Create(t *testing.T) {
+	Create(openDB(), openRedis())
+}
+
+func _test_GetSet(t *testing.T) {
+	u := User{}
+	err := Get(&u, "users", 1, 0)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	println(u.Id, u.Name, u.Descp)
+	err = Get(&u, "users", 1, 0)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	println(u.Id, u.Name, u.Descp)
+
+	println("\n", "set testing")
+	// 更新操作
+	u.Name = "tiege"
+	u.Descp = "我是铁哥"
+	err = Set(&u, "users", 1, 2) // 2秒更新
+	if err != nil {
+		t.Error(err.Error())
+	}
+	//dbc.pool.Get().Do("HDEL", "users", 1)
+	time.Sleep(time.Duration(1) * time.Second)
+	err = Get(&u, "users", 1, 1)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	println(u.Id, u.Name, u.Descp)
+
+	time.Sleep(time.Duration(2) * time.Second)
+	err = Get(&u, "users", 1, 0)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	println(u.Id, u.Name, u.Descp)
+}
+
+func Test_DBC(t *testing.T) {
+	_test_Create(t)
+	_test_GetSet(t)
+}
